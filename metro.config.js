be0cf411ad5f81e88a8a63f7016a2fdf38ab6fd4
petergok <peter@tempolabs.ai>
2025-03-05 17:26:49 -0500

    Updates for different cache

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require("nativewind/metro");
+const { FileStore } = require("@expo/metro-config/file-store");
+const path = require("path");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
+config.cacheStores = [
+  new FileStore({
+    root: path.join(__dirname, "node_modules/.metro-cache"),
+  }),
+];
 
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = withNativeWind(config, { input: "./global.css" });
